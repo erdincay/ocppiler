f28bc5769f34931f82506eb988e86b6640b9b21c
Hadley Luker <lukerhad@grinnell.edu>
2018-02-05 15:58:24 -0600

    added if for characters

--- cppiler.cpp
+++ cppiler.cpp
@@ -1,38 +1,69 @@
+#include <cctype> // for isspace, isdigit
 #include <fstream> // for ifstream
 #include <iostream> // for cout
-#include <string> // for .compare
 #include <vector> // for vector
 
 using namespace std; // unless I use actual libraries
 
-// Expression definitions
-class Exp {
-  virtual void interpret();
+// Expression classes ----------------------------------------------------------
+class Expression {
+public:
+  virtual int interpret() = 0;
 };
 
-class Integer: public Exp {
+class Literal: public Expression {
 public:
-  void interpret();
-};
-
-class Plus: public Exp {
-public:
-  void interpret();
+  Literal (int);
+  int interpret();
 private:
-  Exp left;
-  Exp right;
+  int data;
 };
 
-enum Token { lparen, rparen, plus, integer };
+class Plus: public Expression {
+public:
+  Plus (Expression&, Expression&);
+  int interpret();
+private:
+  Expression *left;
+  Expression *right;
+};
 
-vector<Token> * lex(ifstream& input) {
+// Token classes ---------------------------------------------------------------
+enum TokenKind {
+  INT, PLUS, LPAREN, RPAREN;
+};
+
+class Token {
+public
+};
+
+// Lexer -----------------------------------------------------------------------
+
+vector<Token> lex(ifstream& input) {
   char cur_char;
+  vector<Token> ret;
   while (!input.eof()) {
     input.get(cur_char);
-    cout << cur_char;
+    if (cur_char == '(') {
+      cout << "lparen ";
+    } else if (cur_char == ')') {
+      cout << "rparen ";
+    } else if (cur_char == '+') {
+      cout << "plus ";
+    } else if (isspace(cur_char)) {
+      cout << "space ";
+    } else if (isdigit(cur_char)) {
+      cout << "digit ";
+    } else {
+      cout << "\nYOU DONE FUCKED UP\n";
+    }
   }
-  return NULL;
+  return ret;
 }
+
+// Parser ----------------------------------------------------------------------
+
+// Driver ----------------------------------------------------------------------
 
 int main(int argc, char *argv[]) {
   // Open file.
@@ -49,6 +80,7 @@ int main(int argc, char *argv[]) {
   // Parse it.
 }
 
+// Notes -----------------------------------------------------------------------
 //declare interface Exp
 // with a subclass literal (integer) implements exp, which returns value
 // with a subclass plus implements exp that can have two exp children,
