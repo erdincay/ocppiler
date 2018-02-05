6e0c3852c1ad989329153533e409abfe37c7d0b2
Hadley Luker <lukerhad@grinnell.edu>
2018-02-04 22:20:49 -0600

    Added file reading:

--- cppiler.cpp
+++ cppiler.cpp
@@ -1,3 +1,56 @@
-int main(int argc, char *argv[]) {
-  // TODO: actually add compily things
+#include <fstream> // for ifstream
+#include <iostream> // for cout
+#include <string> // for .compare
+#include <vector> // for vector
+
+using namespace std; // unless I use actual libraries
+
+// Expression definitions
+class Exp {
+  virtual void interpret();
+};
+
+class Integer: public Exp {
+public:
+  void interpret();
+};
+
+class Plus: public Exp {
+public:
+  void interpret();
+private:
+  Exp left;
+  Exp right;
+};
+
+enum Token { lparen, rparen, plus, integer };
+
+vector<Token> * lex(ifstream& input) {
+  char cur_char;
+  while (!input.eof()) {
+    input.get(cur_char);
+    cout << cur_char;
+  }
+  return NULL;
 }
+
+int main(int argc, char *argv[]) {
+  // Open file.
+  ifstream input;
+  input.open(argv[1]);
+  if (!input) {
+    cout << "Unable to open file.\n";
+    exit(EXIT_FAILURE);
+  }
+
+  // Lex it.
+  lex(input);
+
+  // Parse it.
+}
+
+//declare interface Exp
+// with a subclass literal (integer) implements exp, which returns value
+// with a subclass plus implements exp that can have two exp children,
+// both have an intepretation function
+// recursively call interpret on sub functions
